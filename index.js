ce70fc06660acd8c929bbc4518d34725a367b4b8
Ravi teja Darapu <ravitejadarapu@Ravis-MacBook-Pro.local>
2017-06-04 12:08:12 +0530

    index js to serve app/index.html fix2

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ var app = express();
 app.use(parser.urlencoded({extended:false}));
 app.use(parser.json());
 app.listen((process.env.PORT || 5000));
-app.use(express.static(__dirname, '/app'));
+app.use(express.static(__dirname + '/app'));
 
 app.get('/', function(req, res){
     res.render("index");
